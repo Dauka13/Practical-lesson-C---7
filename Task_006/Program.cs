b589934817dc7a63bcc282bc1b894ed605e576f7
Dauka <orazbekovdd13@mail.ru>
2022-09-09 23:20:07 +0600

    add task 006. The smallest line

---
+++ Task_006/Program.cs
@@ -0,0 +1,60 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+int rowsNumber(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int rows = 4;
+int columns = 7;
+int min = 10;
+int max = 100;
+
+int[,] matrix = GetMatrix(rows, columns, min, max);
+PrintMatrix(matrix);
+Console.WriteLine(" ");
+int index = rowsNumber(matrix);
+Console.WriteLine(index);
